8d19439cd1f29d020bfad8ceaeaee0a13430801b
Brett Anthony <brettanthony@outlook.com>
2018-04-03 15:43:41 +1000

    NucliexGui.Harness

---
+++ NuclexGui.Harness.iOS/Main.cs
@@ -0,0 +1,50 @@
+﻿using UIKit;
+using Microsoft.Xna.Framework;
+using System;
+using Foundation;
+
+namespace NuclexGui_Harness.iOS
+{
+    #region Entry Point
+    //[Register("AppDelegate")]
+    //class Program : UIApplicationDelegate
+    //{
+    //    Game game;
+    //    public override void FinishedLaunching(UIApplication application)
+    //    {
+    //        // Fun begins..
+    //        game = new Game1();
+    //        game.Run();
+    //    }
+
+    //    /// <summary>
+    //    /// The main entry point for the application.
+    //    /// </summary>
+    //    static void Main(string[] args)
+    //    {
+    //        UIApplication.Main(args, null, "AppDelegate");
+    //    }
+    //}
+
+    public class Application
+    {
+
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            //Setup our services for core library
+            //SharedServiceRegistrar.Startup();
+
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+    }
+
+
+
+
+    #endregion
+
+}
+
+
